100c564a226340738f40d67009f12d072d055ead
SHREYASH WARALKAR <135358294+Shreyash67@users.noreply.github.com>
2025-04-10 21:53:47 +0530

    Adding index.html and modify app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-import pandas as pd
+import numpy as np
 import pickle
 
 app = Flask(__name__)
@@ -10,6 +10,24 @@ with open('H:\linear_regression\model\Regression_model.pkl','rb') as file:
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        try:
+            Area = float(request.form['Area'])
+            BHK = float(request.form['BHK'])
+            Bathroom = float(request.form['Bathroom'])
+
+            input_data = np.array([[Area,BHK,Bathroom]])
+            result = model.predict(input_data)
+            predict_price = float(result[0])
+
+            return render_template('index.html',result = f"Predicted House Price: {predict_price:.2f}$")
+        except Exception as e:
+            return render_template('index.html',result=f"Error: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
